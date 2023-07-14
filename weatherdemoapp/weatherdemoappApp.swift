cb956fe615029d0bfdf807430680c0ccef020eae
asadbek-ip <dasturchi6678@gmail.com>
2023-07-14 22:54:25 +0500

    Initial Commit

---
+++ weatherdemoapp/weatherdemoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherdemoappApp.swift
+//  weatherdemoapp
+//
+//  Created by Asadbek on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherdemoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
