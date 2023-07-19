f05f35714cd3bb178c113b38e99cda79dd166489
asadbek-ip <dasturchi6678@gmail.com>
2023-07-19 16:37:41 +0500

    first commit

---
+++ weatherdemoapp/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  weatherdemoapp
+//
+//  Created by Asadbek on 15/07/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity,maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
